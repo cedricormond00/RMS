d34d3566ebe6afad5e30d05109769ade0f427f2c
cedricormond00 <72571500+cedricormond00@users.noreply.github.com>
2023-06-22 18:03:31 +0200

    WORKING: did not do any changes. Tried the SMS function. Failed. I need to investigate why. Probably some PIN or SIM card compatibility of some sort

---
+++ final_project/src/main_SMS.cpp
@@ -0,0 +1,80 @@
+// Include the MKRNB library
+#include <Arduino.h>
+#include <MKRNB.h>
+// #include "arduino_secrets.h" 
+// Please enter your sensitive data in the Secret tab or arduino_secrets.h
+// PIN Number
+const char PINNUMBER[] = "";
+
+// initialize the library instance
+NB nbAccess;
+NB_SMS sms;
+//Read input serial
+int readSerial(char result[]) {
+  int i = 0;
+  while (1) {
+    while (Serial.available() > 0) {
+      char inChar = Serial.read();
+      if (inChar == '\n') {
+        result[i] = '\0';
+        Serial.flush();
+        return 0;
+      }
+      if (inChar != '\r') {
+        result[i] = inChar;
+        i++;
+      }
+    }
+  }
+}
+
+void setup() {
+  // initialize serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  Serial.println("SMS Messages Sender");
+
+  // connection state
+  bool connected = false;
+
+  // If your SIM has PIN, pass it as a parameter of begin() in quotes
+  while (!connected) {
+    Serial.println("testing");
+    if (nbAccess.begin(PINNUMBER) == NB_READY) {
+    // if (nbAccess.begin() == NB_READY) {
+      connected = true;
+    } else {
+      Serial.println("Not connected");
+      delay(1000);
+    }
+  }
+
+  Serial.println("NB initialized");
+}
+
+void loop() {
+
+  Serial.print("Enter a mobile number: ");
+  char remoteNum[20];  // telephone number to send sms
+  readSerial(remoteNum);
+  Serial.println(remoteNum);
+
+  // sms text
+  Serial.print("Now, enter SMS content: ");
+  char txtMsg[200];
+  readSerial(txtMsg);
+  Serial.println("SENDING");
+  Serial.println();
+  Serial.println("Message:");
+  Serial.println(txtMsg);
+
+  // send the message
+  sms.beginSMS(remoteNum);
+  sms.print(txtMsg);
+  sms.endSMS();
+  Serial.println("\nCOMPLETE!\n");
+}
+
